b1036aa6324bdb892adeacc6f54b0d329d39d477
vsumit89 <sumit.vishwakarma@factly.in>
2023-08-19 09:52:23 +0530

    added vercel

---
+++ .vercel/output/functions/render.func/dist/chunks/pages/project.astro.178e86ff.mjs
@@ -0,0 +1,19 @@
+import { c as createAstro, a as createComponent, r as renderTemplate } from '../astro.1d7f3216.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const $$Astro = createAstro();
+const $$Project = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Project;
+  return renderTemplate``;
+}, "/Users/sumit/dev/not-factly/knowledge-garden-new/my-site/src/pages/project.astro", void 0);
+
+const $$file = "/Users/sumit/dev/not-factly/knowledge-garden-new/my-site/src/pages/project.astro";
+const $$url = "/project";
+
+export { $$Project as default, $$file as file, $$url as url };
---
+++ .vercel/output/functions/render.func/dist/chunks/project@_@astro.06cc38d6.mjs
@@ -0,0 +1,15 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.1d7f3216.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./pages/project.astro.178e86ff.mjs');
+
+export { page };
